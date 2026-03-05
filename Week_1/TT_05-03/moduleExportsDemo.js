0474133a40c3cd190e491ae584da44c5d710e6f0
Pavan Bojja <pavanbojja33@gmail.com>
2026-03-05 06:33:35 +0530

    Week_1 05-03

---
+++ Week_1/TT_05-03/moduleExportsDemo.js
@@ -0,0 +1,18 @@
+let myName= "Pavan";
+let age=21;
+
+function login(){
+    console.log("Login Successfull")
+}
+
+function logout(){
+    console.log("Logout Successfull")
+}
+
+function display(){
+    console.log(`Name : ${myName}`);
+    console.log(`Age : ${age}`);
+    return 
+}
+
+module.exports = display;
---
+++ Week_1/TT_05-03/sample.js
@@ -0,0 +1,5 @@
+const demo=require('./exportDemo');
+const moduleDemo = require("./moduleExportsDemo")
+
+console.log(`Radius : ${demo.r} -------- Area : ${demo.area()}`)
+moduleDemo()
